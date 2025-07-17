389d7196793c108b417d50ae4d12baf06b0e874c
Sim <simonapometkova@gmail.com>
2025-07-17 15:43:22 +0300

    Add 2D camera movement

---
+++ Assets/Scripts/Gameplay/Camera/CameraController.cs
@@ -0,0 +1,65 @@
+using System;
+using UnityEngine;
+
+namespace Gameplay.Camera
+{
+    /// <summary>
+    /// Controls an orthographic 2D camera for moving around the dungeon.
+    /// Supports movement by mouse position near screen edges and zooming via mouse scroll wheel.
+    /// </summary>
+    public class CameraController : MonoBehaviour
+    {
+        [Header("Settings")] 
+        [SerializeField] private float movementSpeed = 10f;
+        [SerializeField] private float movementEdgeThreshold = 10f;
+        [SerializeField] private float zoomSpeed = 5f;
+        [SerializeField] private float minZoom = 5f;
+        [SerializeField] private float maxZoom = 20f;
+
+        private UnityEngine.Camera _camera;
+
+        private void Start()
+        {
+            _camera = UnityEngine.Camera.main;
+        }
+
+        /// <summary>
+        /// Detects mouse position and scroll input every frame to move and zoom the camera accordingly.
+        /// </summary>
+        
+        // TODO use events instead of checking on every frame to optimize performance.
+        private void Update()
+        {
+            // Camera movement
+            Vector3 movement = Vector3.zero;
+            Vector3 mousePosition = Input.mousePosition;
+
+            // Move camera horizontally if mouse near screen edges
+            if (mousePosition.x >= Screen.width - movementEdgeThreshold)
+                movement.x += 1;
+            else if (mousePosition.x <= movementEdgeThreshold)
+                movement.x -= 1;
+            
+            // Move camera vertically if mouse near screen edges
+            if (mousePosition.y >= Screen.height - movementEdgeThreshold)
+                movement.y += 1;
+            else if (mousePosition.y <= movementEdgeThreshold)
+                movement.y -= 1;
+            
+            movement.Normalize();
+            
+            transform.position += movement * (movementSpeed * Time.deltaTime);
+            
+            // Zoom camera with mouse scroll wheel
+            float scroll = Input.GetAxis("Mouse ScrollWheel");
+
+            if (Math.Abs(scroll) > 0.01f)
+            {
+                _camera.orthographicSize -= scroll * zoomSpeed;
+                
+                // Constrain zoom amount
+                _camera.orthographicSize = Mathf.Clamp(_camera.orthographicSize, minZoom, maxZoom);
+            }
+        }
+    }
+}
